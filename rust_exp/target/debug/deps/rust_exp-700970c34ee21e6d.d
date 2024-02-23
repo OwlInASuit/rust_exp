dd5c56c7da5b3761959f822e3d6b7883711ee54e
OwlInASuit <dgomezaguado@deloitte.es>
2024-02-23 10:39:35 +0100

    Patch_002

---
+++ rust_exp/target/debug/deps/rust_exp-700970c34ee21e6d.d
@@ -0,0 +1,5 @@
+/home/dgaguado/Rust/rust_exp/rust_exp/target/debug/deps/rust_exp-700970c34ee21e6d: src/main.rs
+
+/home/dgaguado/Rust/rust_exp/rust_exp/target/debug/deps/rust_exp-700970c34ee21e6d.d: src/main.rs
+
+src/main.rs:
