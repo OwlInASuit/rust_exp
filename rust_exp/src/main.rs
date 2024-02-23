3bd57094293ff5573c50b798f8c8b452c0782e23
OwlInASuit <dgomezaguado@deloitte.es>
2024-02-23 13:41:08 +0100

    Changes

--- rust_exp/src/main.rs
+++ rust_exp/src/main.rs
@@ -1,5 +1,27 @@
-fn median(a: Vec<f32>) -> Option<f32> {
-    todo!();
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    //todo!(); // comment
+    if a.is_empty(){
+        return None;
+    }
+    //a.sort(); // f32 can't be compared without establishing a criteria
+    //a.sort_by(compare: |x: &f32, y: &f32| {x.partial_cmp(y).unwrap()}); // ???
+    a.sort_by(|x, y| x.partial_cmp(y).expect("Success"));
+
+    //find middle value
+    let n_elements: usize = a.len();
+    let middle: usize = n_elements / 2; // floor
+    let a_is_even = n_elements % 2 == 0;
+
+    // you may declare a thing's value with an if
+    let med : f32 = if a_is_even {
+        // mean  / average of the middle-two elements
+        (a[middle] + a[middle-1]) / 2.0
+    } else {
+        a[middle]
+    };
+
+    Some(med)
+
 }
 
 fn main(){
@@ -12,7 +34,7 @@ fn empty_list(){
     let input: Vec<f32> = vec![];
     let expected_output: Option<f32> = None;
     let actual_output: Option<f32> = median(input);
-    assert_eq!(actual_output, expected_output);
+    assert_eq!(actual_output, expected_output); // pass/fail
 }
 
 #[test]
@@ -20,7 +42,7 @@ fn sorted_list(){
     let input: Vec<f32> = vec![1.0, 4.0, 5.0];
     let expected_output: Option<f32> = Some(4.0);
     let actual_output: Option<f32> = median(input);
-    assert_eq!(actual_output, expected_output);
+    assert_eq!(actual_output, expected_output); // pass/fail
 }
 
 #[test]
@@ -28,7 +50,7 @@ fn even_length(){
     let input: Vec<f32> = vec![1.0, 3.0, 5.0, 6.0];
     let expected_output: Option<f32> = Some(4.0);
     let actual_output: Option<f32> = median(input);
-    assert_eq!(actual_output, expected_output);
+    assert_eq!(actual_output, expected_output); // pass/fail
 }
 
 #[test]
@@ -36,5 +58,6 @@ fn unsorted_list(){
     let input: Vec<f32> = vec![1.0, 5.0, 2.0];
     let expected_output: Option<f32> = Some(2.0);
     let actual_output: Option<f32> = median(input);
-    assert_eq!(actual_output, expected_output);
+    assert_eq!(actual_output, expected_output); // pass/fail
 }
+
