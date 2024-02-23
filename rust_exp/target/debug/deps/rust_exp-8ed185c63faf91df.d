51411b122804e2a4802dd91d35c0ae0d72071980
OwlInASuit <dgomezaguado@deloitte.es>
2024-02-23 10:13:25 +0100

    First commit

---
+++ rust_exp/target/debug/deps/rust_exp-8ed185c63faf91df.d
@@ -0,0 +1,5 @@
+/home/dgaguado/Rust/rust_exp/rust_exp/target/debug/deps/librust_exp-8ed185c63faf91df.rmeta: src/main.rs
+
+/home/dgaguado/Rust/rust_exp/rust_exp/target/debug/deps/rust_exp-8ed185c63faf91df.d: src/main.rs
+
+src/main.rs:
